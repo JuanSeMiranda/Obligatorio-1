c31945f2ef406c33abbb17ef58d2c3367eaea865
JusnSeMiranda <seba-m@hotmail.com>
2021-03-26 15:10:12 -0300

    se agrega barco pasajeros

---
+++ DtBarcoPajajeros.h
@@ -0,0 +1,22 @@
+#ifndef DTBARCOPASAJEROS
+#define DTBARCOPASAJEROS
+#include "TipoTamanio.h"
+#include "DtBarco.h"
+
+class DtBarcoPasajeros : public DtBarco{
+private:
+    int cantPasajeros;
+    TipoTamanio tamanio;
+public:
+    DtBarcoPasajeros();
+    DtBarcoPasajeros(string nombre, string id, int cantPasajeros, TipoTamanio tamanio);
+    void setCantPasajeros();
+    int getCantPasajeros();
+    void setTamanio();
+    TipoTamanio getTamanio();
+    ~DtBarcoPasajeros();
+};
+
+
+
+#endif
