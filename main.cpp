b7025dc225fdf59873ebbea0bab3900715ffb3f5
AgusRamos2002 <81432262+AgusRamos2002@users.noreply.github.com>
2021-03-30 18:55:07 -0300

    Funciones en el main.cpp
    Se hicieron las funciones 4 y 7 del laboratorio junto a sus menús, además de que se completo el menú de la segunda función.

--- main.cpp
+++ main.cpp
@@ -82,8 +82,8 @@ void menuAgregarPuerto(){
 void agregarBarco(DtBarco& barco){
     try{
         DtBarcoPasajeros& dtBarcoPas = dynamic_cast<DtBarcoPasajeros&>(barco);
-        BarcoPasajeros* barcoPas = new BarcoPasajeros(dtBarcoPas.getNombre(),
-                                                      dtBarcoPas.getId(),
+        BarcoPasajeros* barcoPas = new BarcoPasajeros(dtBarcoPas.getId(),
+                                                      dtBarcoPas.getNombre(),
                                                       dtBarcoPas.getCantPasajeros(),
                                                       dtBarcoPas.getTamanio());
         colBarcos.b[colBarcos.tope] = barcoPas;
@@ -91,8 +91,8 @@ void agregarBarco(DtBarco& barco){
     } catch(bad_cast){
         try{
             DtBarcoPesquero& dtBarcoPes = dynamic_cast<DtBarcoPesquero&>(barco);
-            BarcoPesquero* barcoPes = new BarcoPesquero(dtBarcoPes.getNombre(),
-                                                        dtBarcoPes.getId(),
+            BarcoPesquero* barcoPes = new BarcoPesquero(dtBarcoPes.getId(),
+                                                        dtBarcoPes.getNombre(),
                                                         dtBarcoPes.getCapacidad(),
                                                         dtBarcoPes.getCarga());
             colBarcos.b[colBarcos.tope] = barcoPes;
@@ -106,42 +106,58 @@ void menuAgregarBarco(){
     string nombre;
     int opcion;
 
-    cout << "Ingrese ID:";
+    cout << "Ingrese ID: ";
     cin >> id;
 
-    cout << "Ingrese nombre:";
+    int i = 0;
+    while(i < colBarcos.tope && colBarcos.b[i]->getId() != id)
+        i++;
+
+
+    if(i != colBarcos.tope)
+        throw invalid_argument("Ya existe un barco con ese Id");
+
+
+    cout << "Ingrese nombre: ";
     cin >> nombre;
 
-    cout << "Elija tipo de barco (1 para pasajero, 2 para carga)";
-/*
+    cout << "Elija tipo de barco (1 para pasajero, 2 para carga): ";
+    cin >> opcion;
+
     switch(opcion){
-        case 1: int cantPasajeros;
-                string tamanio;
-                cout << "Ingrese cantidad de pasajeros:";
-                cin >> cantPasajeros;
+        case 1: {
+                    int cantPasajeros;
+                    int tamanioNum;
+                    TipoTamanio tamanio;
+                    cout << "Ingrese cantidad de pasajeros: ";
+                    cin >> cantPasajeros;
 
-                cout << "Ingrese tipo tamanio:";
-                cin >> tamanio;
+                    cout << "Ingrese tipo tamanio (0 para bote, 1 para crucero, 2 para galeon o 3 para transatlantico): ";
+                    cin >> tamanioNum;
 
-                TipoTamanio& tam = dynamic_cast<TipoTamanio&>(tamanio);
+                    tamanio = (TipoTamanio) tamanioNum;
 
-                DtBarcoPasajeros barco(id, nombre, cantPasajeros, tamanio);
-                agregarBarco(barco);
+                    DtBarcoPasajeros barco(id, nombre, cantPasajeros, tamanio);
+                    agregarBarco(barco);
+                }
                 break;
+        case 2: {
+                    int capacidad;
+                    int carga;
+                    cout << "Ingrese capacidad: ";
+                    cin >> capacidad;
 
-        case 2: int capacidad;
-                int carga;
-                cout << "Ingrese capacidad:";
-                cin >> capacidad;
+                    cout << "Ingrese carga: ";
+                    cin >> carga;
 
-                cout << "Ingrese carga:";
-                cin >> carga;
-
-                DtBarcoPesquero barco(id, nombre, capacidad, carga);
-                agregarBarco(barco);
+                    DtBarcoPesquero barco(id, nombre, capacidad, carga);
+                    agregarBarco(barco);
+                }
+                break;
+        default:cout << "Opcion invalida, vuelva a intentarlo." << endl;
                 break;
     }
-*/
+
 }
 
 //TERCERA FUNCION
@@ -177,6 +193,113 @@ void menuListarPuertos(){
     }
 }
 
+//CUARTA FUNCION
+
+void agregarArribo(string idPuerto, string idBarco, DtFecha fecha, int cargaDespacho){
+	int i = 0;
+	while(colPuertos.p[i]->getId() != idPuerto && i != colPuertos.tope)
+		i++;
+
+    if(i == colPuertos.tope && colPuertos.p[i]->getId() != idPuerto)
+        throw invalid_argument("No existe el puerto con ese Id.\n");
+
+	int j = 0;
+	while(colBarcos.b[j]->getId() != idBarco && j != colBarcos.tope)
+		j++;
+
+    if(j == colBarcos.tope && colBarcos.b[j]->getId() != idBarco)
+        throw invalid_argument("No existe el barco con ese Id.\n");
+
+	Arribo* arribo = new Arribo(fecha, cargaDespacho);
+
+	colPuertos.p[i]->agregarArribo(arribo);
+}
+
+void menuAgregarArribo(){
+    string idPuerto;
+    string idBarco;
+    int cargaDespacho;
+
+    cout << "Ingrese el Id del Puerto" << endl;
+    cin >> idPuerto;
+
+    cout << "Ingrese el Id del Barco" << endl;
+    cin >> idBarco;
+
+    int dia, mes, anio;
+
+    cout << "Ingrese el dia" << endl;
+    cin >> dia;
+
+    cout << "Ingrese el mes" << endl;
+    cin >> mes;
+
+    cout << "Ingrese el anio" << endl;
+    cin >> anio;
+
+    DtFecha fecha(dia, mes, anio);
+
+    cout << "Ingrese la carga a despachar" << endl;
+    cin >> cargaDespacho;
+
+    agregarArribo(idPuerto, idBarco, fecha, cargaDespacho);
+
+}
+
+//SEPTIMA FUNCION
+
+DtBarco** listarBarcos(int& cantBarcos){
+  cantBarcos=colBarcos.tope;
+  DtBarco** listado = new DtBarco*[cantBarcos];
+  for(int i=0 ; i < colBarcos.tope ; i++){
+    if(BarcoPasajeros* pas = dynamic_cast<BarcoPasajeros*>(colBarcos.b[i])){
+        DtBarcoPasajeros* dtbpa = new DtBarcoPasajeros(pas->getId(), pas->getNombre(), pas->getCantPasajeros(), pas->getTamanio());
+        listado[i]=dtbpa;
+
+    } else if(BarcoPesquero* pes= dynamic_cast<BarcoPesquero*>(colBarcos.b[i])){
+        DtBarcoPesquero* dtbpe = new DtBarcoPesquero(pes->getId(), pes->getNombre(), pes->getCapacidad(), pes->getCarga());
+
+        listado[i]=dtbpe;
+    }
+  }
+  return listado;
+}
+
+void imprimirTamanio(TipoTamanio num){
+    switch(num){
+        case BOTE: cout << "BOTE" << endl;
+                break;
+        case CRUCERO: cout << "CRUCERO" << endl;
+                break;
+        case GALEON: cout << "GALEON" << endl;
+                break;
+        case TRANSATLANTICO: cout << "TRANSATLANTICO" << endl;
+                break;
+    }
+}
+
+void menuListarBarcos(){
+    DtBarco** listado;
+    listado = listarBarcos(colBarcos.tope);
+    for(int i = 0; i < colBarcos.tope; i++){
+        cout << "\n" << endl;
+        cout << "Barco numero " << i+1 << endl;
+        cout << "----------------" << endl;
+        if(BarcoPasajeros* pas = dynamic_cast<BarcoPasajeros*>(colBarcos.b[i])){
+            cout << "Id del barco: " << pas->getId() << endl;
+            cout << "Nombre del barco: " << pas->getNombre() << endl;
+            cout << "Cantidad de pasajeros del barco: " << pas->getCantPasajeros() << endl;
+            cout << "Tipo de barco: ";
+            imprimirTamanio(pas->getTamanio());
+        }else if(BarcoPesquero* pes= dynamic_cast<BarcoPesquero*>(colBarcos.b[i])){
+            cout << "Id del barco: " << pes->getId() << endl;
+            cout << "Nombre del barco: " << pes->getNombre() << endl;
+            cout << "Capacidad del barco: " << pes->getCapacidad() << endl;
+            cout << "Carga del barco: " << pes->getCarga() << endl;
+        }
+    }
+}
+
 /////////////////////////
 
 //Menu y main
@@ -203,7 +326,7 @@ int main(){
     colBarcos.tope = 0;
 
     int opcion;
-    while(opcion != 7){
+    while(opcion != 8){
         switch(opcion){
             case 1: try{
                         menuAgregarPuerto();
@@ -212,6 +335,26 @@ int main(){
                         cout << fecha.what() << endl;
                         cout << "Ya maneje la excepcion" << endl;
                     }
+                    break;
+            case 2: try{
+                        menuAgregarBarco();
+                    } catch(invalid_argument& fecha){
+                        cout << fecha.what() << endl;
+                    }
+                    break;
+            case 3: menuListarPuertos();
+                    break;
+            case 4: try{
+                        menuAgregarArribo();
+                    } catch(invalid_argument& fecha){
+                        cout << fecha.what() << endl;
+                    } catch (invalid_argument& BarcoPesquero){
+                        cout << BarcoPesquero.what() << endl;
+                    }
+                    break;
+            case 7: menuListarBarcos();
+                    break;
+            default: cout << "Opcion invalida, vuelva a intentarlo" << endl;
                     break;
         }
         menu();
